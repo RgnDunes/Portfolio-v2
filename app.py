7c97973922d982c7a25d7729b0204c47dc979244
RgnDunes <singh.divyansh1802@gmail.com>
2021-03-09 01:32:35 +0530

    Added Hobby section, resume section, contact me section

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask,render_template,redirect,flash,url_for,request,session
 from flask_sqlalchemy import SQLAlchemy
 import os
+import smtplib, ssl
+from datetime import date
 
 # CONFIG
 app=Flask(__name__)
@@ -28,9 +30,9 @@ def certificates():
 def projects():
     return render_template('projects.html')
 
-@app.route('/gallery', methods=['GET', 'POST'])
-def gallery():
-    return render_template('gallery.html')
+@app.route('/hobby', methods=['GET', 'POST'])
+def hobby():
+    return render_template('hobby.html')
 
 @app.route('/resume', methods=['GET', 'POST'])
 def resume():
@@ -39,6 +41,41 @@ def resume():
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
     return render_template('contact.html')
+
+@app.route('/contact/sendingFeedback', methods=['GET', 'POST'])
+def sendingFeedback():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        subject = request.form.get('subject')
+        msg = request.form.get('message')
+        today = date.today()
+        curr_date = today.strftime("%B %d, %Y")
+        port = 465  # For SSL
+        smtp_server = "smtp.gmail.com"
+        receiver_email = email  # Enter receiver address
+        sender_email = "testingdunes@gmail.com"  # Enter your address
+        password = "Duqo&@5200"
+        message = """\
+        Subject: Thank you for the feedback.
+
+        We will reach out to you soon."""
+
+        context = ssl.create_default_context()
+        with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+            server.login(sender_email, password)
+            server.sendmail(sender_email, receiver_email, message)
+        
+        receiver_email = "testingdunes@gmail.com"
+        message = """\
+        Subject: Portfolio-v2.
+
+Feedback from """ + email + """. \nMessage : """ + msg + """"\nDate : """ + curr_date
+
+        context = ssl.create_default_context()
+        with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+            server.login(sender_email, password)
+            server.sendmail(sender_email, receiver_email, message)
+    return render_template('index.html')
 
 # MAIN
 if __name__=='__main__':
